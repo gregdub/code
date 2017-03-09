75055a86040415c8ee7d66afe6a63d39a53a7481
gregdub <gregoire.dubois@master-stic.fr>
2017-03-09 13:20:03 +0100

    Update main.py
    ajout des boutons et champs de saisie, champ d'affichage réception

--- main.py
+++ main.py
@@ -28,27 +28,138 @@ class MainFrame(wx.Frame):
     def __init__(self):
         """Initialisation de la class"""
         # Configuration de la fenêtre (taille, nom, ...)
-        wx.Frame.__init__(self, None, -1, title='Liaison série RS232'
-                          , style = wx.SYSTEM_MENU | wx.CAPTION | wx.CLOSE_BOX, size = (800, 400))
-
+        wx.Frame.__init__(self, None, -1, title='Liaison serie RS232'
+                          , style = wx.SYSTEM_MENU | wx.CAPTION | wx.CLOSE_BOX, size = (800, 600))
+        self.Centre()
         # Evenement lors de la fermeture de la fenêtre :
         # self.Bind(wx.EVT_CLOSE, self.OnFerme)
 
         self.panel = wx.Panel(self)
 
-        wx.StaticBox(self.panel,1," Configuration : ",(10,10),size=(400,200))
+        font = wx.SystemSettings_GetFont(wx.SYS_SYSTEM_FONT)
+        font.SetPointSize(10)
+        
+        wx.StaticBox(self.panel,1," Configuration du port d'emission: ",(10,10),size=(300,100))
 
         # Modification du port :
         wx.StaticText(self.panel,-1,"Port :",(20,30))
-        ListPort = ['COM1','COM2','COM3']
-        wx.ComboBox(self.panel,-1,choices=ListPort,size=(80,26),pos=(150,30))
+        ListPort = ['COM1','COM2','COM3','COM4','COM5','COM6','COM7','COM8','COM9','COM10']
+        wx.ComboBox(self.panel,-1,choices=ListPort,size=(80,26),pos=(100,30))
 
         # Vitesse de transmission
         wx.StaticText(self.panel,-1,"Vitesse :",(20,50))
-        ListVitesse = ['2400','4800','9600','19200','38400','56000']
-        wx.ComboBox(self.panel,-1,choices=ListVitesse,size=(80,26),pos=(150,50))
+        ListVitesse = ['2400','4800','9600','19200','38400','56000','64000']
+        wx.ComboBox(self.panel,-1,choices=ListVitesse,size=(80,26),pos=(100,50))
+
+        # Boutton de test de la communication serie
+        BoutonTest=wx.Button(self.panel,label="Test COM",pos=(200,30),size=(80,30))
+        self.Bind(wx.EVT_BUTTON, self.TestCom, BoutonTest)
+
+     
+    # ***** PARTIE RECEPTION *****
+          
+        wx.StaticBox(self.panel,1," Configuration du port de reception: ",(360,10),size=(300,100))
+
+        # Modification du port :
+        wx.StaticText(self.panel,-1,"Port :",(370,30))
+        ListPort2 = ['COM1','COM2','COM3','COM4','COM5','COM6','COM7','COM8','COM9','COM10']
+        wx.ComboBox(self.panel,-1,choices=ListPort2,size=(80,26),pos=(450,30))
+
+        # Vitesse de transmission
+        wx.StaticText(self.panel,-1,"Vitesse :",(370,50))
+        ListVitesse2 = ['2400','4800','9600','19200','38400','56000','64000']
+        wx.ComboBox(self.panel,-1,choices=ListVitesse2,size=(80,26),pos=(450,50))
 
 
+    # ***** PARTIE SAISIE TEXTE *****
+
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        hbox1 = wx.BoxSizer(wx.HORIZONTAL)
+
+        #vbox.Add((-10, 10))
+        vbox.AddSpacer(120)
+        
+        """l1 = wx.StaticText(self.panel, -1, "Saisie de la phrase :") 
+		
+        hbox1.Add(l1, 1, wx.EXPAND|wx.ALIGN_LEFT|wx.ALL,5) 
+        self.t1 = wx.TextCtrl(self.panel, size = (600,100), style=wx.TE_MULTILINE) 
+		
+        hbox1.Add(self.t1,1,wx.EXPAND|wx.ALIGN_LEFT|wx.ALL,5) 
+        self.t1.Bind(wx.EVT_TEXT_ENTER,self.SaisiePhrase) 
+        vbox.Add(hbox1)"""
+
+        hbox1 = wx.BoxSizer(wx.HORIZONTAL)
+        st1 = wx.StaticText(self.panel, label='Saisie de la phrase :')
+        hbox1.Add(st1)
+        vbox.Add(hbox1, flag=wx.LEFT | wx.TOP)
+
+        #vbox.Add((-1, 10))
+
+        hbox10 = wx.BoxSizer(wx.HORIZONTAL)
+        self.t20 = wx.TextCtrl(self.panel, size = (600,100), style=wx.TE_MULTILINE)
+        hbox10.Add(self.t20,1,wx.EXPAND|wx.ALIGN_LEFT|wx.ALL,5) 
+        vbox.Add(hbox10) 
+        self.t20.Bind(wx.EVT_TEXT_ENTER,self.SaisieModification)
+
+
+        
+        
+    # ***** PARTIE MODIFICATION DU TEXTE *****
+
+        hbox2 = wx.BoxSizer(wx.HORIZONTAL)
+        st2 = wx.StaticText(self.panel, label='Code a envoyer :')
+        hbox2.Add(st2)
+        vbox.Add(hbox2, flag=wx.LEFT | wx.TOP)
+
+        #vbox.Add((-1, 10))
+
+        hbox3 = wx.BoxSizer(wx.HORIZONTAL)
+        self.t2 = wx.TextCtrl(self.panel, size = (600,100), style=wx.TE_MULTILINE)
+        hbox3.Add(self.t2,1,wx.EXPAND|wx.ALIGN_LEFT|wx.ALL,5) 
+        vbox.Add(hbox3) 
+        self.t2.Bind(wx.EVT_TEXT_ENTER,self.SaisieModification)
+
+        #vbox.Add((-1, 25))
+    
+    
+    # ***** RECEPTION DU CODE *****
+
+        hbox4 = wx.BoxSizer(wx.HORIZONTAL)
+        st3 = wx.StaticText(self.panel, label='Code recu :')
+        hbox4.Add(st3)
+        vbox.Add(hbox4, flag=wx.LEFT | wx.TOP)
+
+        #vbox.Add((-1, 10))
+
+        hbox5 = wx.BoxSizer(wx.HORIZONTAL)
+        self.t3 = wx.TextCtrl(self.panel, size = (600,100), style=wx.TE_MULTILINE)
+        hbox5.Add(self.t3,1,wx.EXPAND|wx.ALIGN_LEFT|wx.ALL,5) 
+        vbox.Add(hbox5) 
+
+
+        # a mettre pour la répartition des zones de texte sur l'ecran
+        self.panel.SetSizer(vbox)
+
+
+        # Boutton pour la transmission
+        BoutonTransmit=wx.Button(self.panel,label="Envoi",pos=(700,500),size=(80,30))
+        self.Bind(wx.EVT_BUTTON, self.Transmit, BoutonTransmit)
+
+    def TestCom(self,event):
+        self.Close(True)
+
+    def SaisiePhrase(self,event): 
+        print "Enter pressed"
+
+    # ***** Fonction ENVOI DU CODE *****
+    # par appui sur Enter ou bouton
+    def SaisieModification(self,event): 
+        print "Enter pressed"
+
+    def Transmit(self,event): 
+        print "Enter pressed"
+
+        
 """
     def OnFerme(self, event):
         Lors de la fermeture de la fenêtre
