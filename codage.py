5eb60f791119b5ecef4c9ab5ef6aa7a48242d9f7
Toladar <Toladar@users.noreply.github.com>
2017-03-09 08:30:38 +0100

    Nouveau fichier : - codage.py 		  - decodage.py
    main.py : Ajout du label pour le port
              Ajout du label pour la vitesse
              Ajout de la combobox pour le port
              Ajout de la combobox pour la vitesse

---
+++ codage.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+################################################################################
+###
+###     Programme : Liaison série RS232 avec code correcteur d'erreur
+###
+###     Description : Implémentation d'un code correcteur d'erreur pour une
+###                   liaison série capable de corriger deux erreurs par octets
+###
+###     Notes : ///////
+###
+###     Auteurs : Toladar
+###               gregdub
+###
+###     Date : 23/02/17
+###
+################################################################################
+"""
+
+REGISTRE=[0,0,0]
+
+C1 = REGISTRE[0] ^ REGISTRE[1] ^ REGISTRE[2]
+print C1
--- main.py
+++ main.py
@@ -32,17 +32,33 @@ class MainFrame(wx.Frame):
                           , style = wx.SYSTEM_MENU | wx.CAPTION | wx.CLOSE_BOX, size = (800, 400))
 
         # Evenement lors de la fermeture de la fenêtre :
-        self.Bind(wx.EVT_CLOSE, self.OnFerme)
+        # self.Bind(wx.EVT_CLOSE, self.OnFerme)
 
+        self.panel = wx.Panel(self)
+
+        wx.StaticBox(self.panel,1," Configuration : ",(10,10),size=(400,200))
+
+        # Modification du port :
+        wx.StaticText(self.panel,-1,"Port :",(20,30))
+        ListPort = ['COM1','COM2','COM3']
+        wx.ComboBox(self.panel,-1,choices=ListPort,size=(80,26),pos=(150,30))
+
+        # Vitesse de transmission
+        wx.StaticText(self.panel,-1,"Vitesse :",(20,50))
+        ListVitesse = ['2400','4800','9600','19200','38400','56000']
+        wx.ComboBox(self.panel,-1,choices=ListVitesse,size=(80,26),pos=(150,50))
+
+
+"""
     def OnFerme(self, event):
-        """ Lors de la fermeture de la fenêtre """
+        Lors de la fermeture de la fenêtre
         dialogue = wx.MessageDialog(self, "Souhaitez vous vraiment quitter cette application ?"
                                     ,"Quitter l'application", wx.OK|wx.CANCEL|wx.ICON_QUESTION)
         result = dialogue.ShowModal()
         dialogue.Destroy()
         if result == wx.ID_OK:
             self.Destroy()
-
+"""
 if __name__ == '__main__':
     APP = wx.App(redirect=True)
     FRAME = MainFrame()
