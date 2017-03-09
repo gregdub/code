c67da337717e8cfd1cdaabc95598303862e79f1f
Toladar <Toladar@users.noreply.github.com>
2017-03-09 09:17:01 +0100

    codage.py : - Ajout de la fonction de décalage 	    - Ajout de la fonction de codage convolutif

--- codage.py
+++ codage.py
@@ -14,12 +14,50 @@
 ###     Auteurs : Toladar
 ###               gregdub
 ###
-###     Date : 23/02/17
+###     Date : 09/03/17
 ###
 ################################################################################
 """
 
+# Initialisation du registre
 REGISTRE=[0,0,0]
 
-C1 = REGISTRE[0] ^ REGISTRE[1] ^ REGISTRE[2]
-print C1
+# Code envoyé
+CODE = "1001"
+
+# Chaine de caractère de retour
+RETOUR = ""
+
+def codeConvolutif():
+    """ Fonction de génération """
+    # Premier polynome générateur
+    c1 = REGISTRE[0] ^ REGISTRE[1] ^ REGISTRE[2]
+    # Second polynome générateur
+    c2 = REGISTRE[0] ^ REGISTRE[2]
+
+    # Envoi de la chaine codé
+    return str(c1) + str(c2)
+
+def decalage(x):
+    """ Fonction de décalage du registre """
+    #Ajout de la nouvelle valeur
+    REGISTRE.append(x)
+
+    # Décalage du registre
+    REGISTRE[0]=REGISTRE[1]
+    REGISTRE[1]=REGISTRE[2]
+    REGISTRE[2]=REGISTRE[3]
+
+    # Suppresion de la valeur
+    del REGISTRE[3]
+
+# Rajout de deux bits à zéro pour le réinitialisation
+CODE = CODE + "00"
+
+# Parcours du Code
+for bit in CODE:
+    decalage(int(bit))
+    RETOUR = RETOUR + str(codeConvolutif())
+
+# Affichage du retour
+print RETOUR
